03817ef6ad42372d3dd84a985bc154d0ea644ac4
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-03 13:32:48 +0900

    21-03-03/13:32 1. 임시로 헤더 *

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const agent = new https.Agent({
   rejectUnauthorized: false
 });
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+})
+
 app.get('/loa-hands/homeData', (req, res) => {
   axios({
     url : 'https://m-lostark.game.onstove.com/News/Event/Now',
