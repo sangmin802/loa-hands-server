c51ce9d018978b8ed092c6edbbf3ab04d7e35294
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-03 14:01:45 +0900

    21-03-03/14:01 1. 유저정보 얻는 방식 추가

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const {JSDOM} = require('jsdom');
 const app = express();
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = 3001
 
 const axios = require('axios');
 const https = require('https');
@@ -37,21 +39,31 @@ app.get('/loa-hands/timer', (req, res) => {
   })
 })
 
-// app.post('/loa-hands/userInfo', (req, res) => {
-//   const name = req.body.name;
-//   const encoded = encodeURIComponent(name);
-//   const userData = await fetch(baseUrl+'Character/'+encoded)
-//   .then(httpRes => httpRes.text());
+app.post('/loa-hands/userInfo', async (req, res) => {
+  const baseUrl = 'https://m-lostark.game.onstove.com/Profile/';
 
-//   axios({
-//     url : 'http://m.inven.co.kr/lostark/timer/',
-//     method : 'GET',
-//     httpsAgent : agent
-//   })
-//   .then(({data}) => {
-//     res.send(data)
-//   })
-// })
+  const name = req.body.name;
+  const encoded = encodeURIComponent(name);
+  const {data : info} = await axios({
+    url : `${baseUrl}Character/${encoded}`,
+    method : 'GET',
+    httpsAgent : agent
+  });
+  if(info.includes("alert('캐릭터 정보가 없습니다.")) res.send('noChar');
+
+  const doc = new JSDOM(info);
+
+  const script = doc.window.document.body.getElementsByTagName('script');
+  const [,memberNo,,pcId,,worldNo] = script[10]?.textContent?.split('\'') ?? null;
+
+  const {data : col} = await axios({
+    url : `${baseUrl}GetCollection?${new URLSearchParams({memberNo, pcId, worldNo}).toString()}`,
+    method : 'GET',
+    httpsAgent : agent
+  });
+  
+  res.send({info, col})
+})
 
 app.listen(port, () => {
   console.log('Server is connected on port'+port)
