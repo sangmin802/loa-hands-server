e7f7193ea831de0e4aa19c94c25810fe40d02759
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-03 15:15:10 +0900

    Access-Control-Allow-Headers

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended : true}))
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
 
